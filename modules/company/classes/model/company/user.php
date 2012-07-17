10beb7bd17c580790d3399054616f66435c02686
Lester Soriano <lestersoriano@yahoo.com>
2012-07-17 16:40:39 +0800

    user

--- modules/company/classes/model/company/user.php
+++ modules/company/classes/model/company/user.php
@@ -28,7 +28,7 @@ class Model_Company_User extends Model_Auth_User
 
 	// This class can be replaced or extended
 
-		/**
+	/**
 	 * Complete the login for a user by incrementing the logins and saving login timestamp
 	 *
 	 * @return void
@@ -38,10 +38,57 @@ class Model_Company_User extends Model_Auth_User
 		if ($this->_loaded)
 		{
 			
-		
 		}
 	}
 	
+	
+	public function build($form);
+	{
+		$post = Validation::factory($form);
+			
+		$rules = $this->rules();
+		
+		foreach($rules as $field => $rule){
+			$post->rules($field, $rule);
+		}
+		
+		
+		if($post->check()){
+			$client = \orm::factory("client")->detect();
+		
+			$user = orm::factory("user");
+			$user->username = !empty($form["email"]) ? $form["email"] : "";
+			$user->email = !empty($form["email"]) ? $form["email"] : "";
+			$user->password = !empty($form["password"]) ? $form["password"] : "";
+			$user->client_id = $client->id;
+			$user->full_name = !empty($form["full_name"]) ? $form["full_name"] : "";
+			$user->first_name = !empty($form["first_name"]) ? $form["first_name"] : "";
+			$user->last_name = !empty($form["last_name"]) ? $form["last_name"] : "";
+			$user->image_url = !empty($form["image_url"]) ? $form["image_url"] : "";
+			$user->slug = !empty($form["slug"]) ? $form["slug"] : "";
+			$user->is_active = 1;
+			$user->date_last_login = \db::expr("now()");
+			$user->save();
+			
+		}
+		
+	}
+	
+	
+	
+	public function save(Validation $validation = NULL)
+	{
+		if(empty($this->_changed['email'])){
+			$this->username = $this->email;
+		}
+
+		$object = parent::save($validation);
+		
+		
+		return $object;
+    }
+    
+
 	
 	/**
      * Check email exists or not
